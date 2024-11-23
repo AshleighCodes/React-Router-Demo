de877ab238583a9fc8ebc4d90e0c35746926a2b0
Ashleigh Wilson <ashleigh.morgan.wilson@gmail.com>
2024-11-23 12:39:32 +1000

    Completed routing - Start from Step 5 (Linking Pages)

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Here we import out pages
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+
+// Here we create our router and tell it whats pages to render at what path
+const router = createBrowserRouter([
+  // These are the three routes!
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Here we wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
